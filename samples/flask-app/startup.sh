65f0c680afe4e147439e5fb06dd3470f9cbec16a
Euan Jonker <euan@unomena.com>
2025-08-20 19:22:23 +0200

    Remove unused startup.sh scripts from all sample apps
    - Removed startup.sh files from django-minimal, flask-app, and django-celery-app
    - Removed mount entries for startup.sh in lxc-compose.yml files
    - Removed chmod +x /startup.sh commands from post_install sections
    - Supervisord is started directly without intermediate scripts
    - Also reorganized django-celery-app configs to use sample-* prefixes

--- samples/flask-app/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Startup script for Flask app container
-# This script is executed when the container starts
-
-# Start supervisord which will manage Redis and Flask
-exec /usr/bin/supervisord -n -c /etc/supervisord.conf
